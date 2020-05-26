367fc34c5b32c11ba5eda57fcac037ef0af953fd
Mannong Pang <pang@carnegiehub.org>
2020-05-26 11:00:29 -0700

    Search and filter posts

--- functions.php
+++ functions.php
@@ -6,9 +6,92 @@ function college_theme_child_style() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/static/css/style.min.css' );
     //Customized Style
     wp_enqueue_style( 'child-style',  get_stylesheet_directory_uri() . '/style.css');
+    if(strpos(trim( $_SERVER["REQUEST_URI"] , '/' ), 'database')) {
+      wp_enqueue_style( 'dt-style',  get_stylesheet_directory_uri() . '/css/jquery.dataTables.min.css');
+    }
+    
+    //load scripts
+    if(strpos(trim( $_SERVER["REQUEST_URI"] , '/' ), 'database')) {
+      wp_enqueue_script( 'dtscript', get_stylesheet_directory_uri() . '/js/jquery.dataTables.min.js', array ( 'jquery' ), true);
+      wp_enqueue_script( 'search_database', get_stylesheet_directory_uri() . '/js/page-data.js', array ( 'dtscript' ), 1.1, true);
+      wp_localize_script( 'search_database', 'ajax_url', admin_url('admin-ajax.php?action=search_database') );
+    }
 }
 
+// Ajax Callback  
+add_action('wp_ajax_search_database', 'search_database_callback');
+add_action('wp_ajax_nopriv_search_database', 'search_database_callback');
+  
+function search_database_callback() {
+  
+    header("Content-Type: application/json"); 
+  
+    if(!isset($_GET['categories'])){
+      $args = array(
+        'posts_per_page'   => -1,
+        'post_type'        => 'post',
+      );
+    }
+    else{
+      $args = array(
+        'posts_per_page'   => -1,
+        'post_type'        => 'post',
+        'tax_query' => array(
+          array(
+              'taxonomy' => 'category',
+              'field' => 'term_id',        
+              'terms' => $_GET['categories'],               
+          )
+        )
+      );
+    }
 
+
+    // $args = array(
+    //   'posts_per_page'   => -1,
+    //   'post_type' => 'post',
+    //   'post_status' => 'publish',
+    //   'tax_query' => array(
+    //     'relation' => 'OR',
+    //     array(
+    //       'taxonomy' => 'post_tag',
+    //       'field' => 'slug',
+    //       'terms' => 'live',
+    //     ),
+    //     array(
+    //       'taxonomy' => 'category',
+    //       'field' => 'slug',
+    //       'terms' => 'candy',
+    //     ),
+    //   ),
+    //   'orderby' => 'date',
+    //   'order' => 'DESC'
+    // );
+
+    
+    $post_query = new WP_Query( $args );
+    $totalData = $post_query->found_posts;
+    
+    $data = array();
+      if ($post_query->have_posts()) {
+          
+          while ($post_query->have_posts()) {
+              $post_query->the_post();
+              $nestedData = array();
+              $nestedData[] = "<a href='" . get_permalink() . "'>" . get_the_title() . "</a>";
+              $nestedData[] = wp_trim_words(strip_tags(get_the_excerpt()), 10);
+              $data[] = $nestedData;
+          }
+          wp_reset_query();
+      }
+      $json_data = array(
+        //"recordsTotal" => intval($totalData),
+        "data" => $data
+      );
+      echo json_encode($json_data);
+
+    wp_die();
+}
 
 add_action( 'init', 'cp_change_post_object' );
 // Change dashboard Posts to Data
